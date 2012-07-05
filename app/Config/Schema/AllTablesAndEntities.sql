e5b9091353f4aa5f71103d2293cdffc1f641d05b
Matthieu Riolo <matthieu.riolo@googlemail.com>
2012-07-05 10:36:40 +0200

    added backup and sql statements

--- app/Config/Schema/AllTablesAndEntities.sql
+++ app/Config/Schema/AllTablesAndEntities.sql
@@ -1,14 +1,9 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`groups`
+-- Table `groups`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`groups` (
+CREATE  TABLE IF NOT EXISTS `groups` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(100) NOT NULL ,
   `created` DATETIME NULL DEFAULT NULL ,
@@ -17,15 +12,16 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`groups` (
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`users`
+-- Table `users`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`users` (
+
+CREATE  TABLE IF NOT EXISTS `users` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(30) NOT NULL DEFAULT 'Gast' ,
   `password` VARCHAR(32) NOT NULL ,
   `email` VARCHAR(50) NOT NULL ,
-  `created` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ,
-  `modified` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ,
+  `created` DATETIME NULL DEFAULT NULL, -- CURRENT_TIMESTAMP ,
+  `modified` DATETIME NULL DEFAULT NULL, -- CURRENT_TIMESTAMP ,
   `score` INT NULL DEFAULT 0 ,
   `isMale` TINYINT NULL DEFAULT 1 ,
   `group_id` INT NOT NULL ,
@@ -33,56 +29,54 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`users` (
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_users_groups`
     FOREIGN KEY (`group_id` )
-    REFERENCES `mydb`.`groups` (`id` )
+    REFERENCES `groups` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_users_groups` ON `mydb`.`users` (`group_id` ASC) ;
+-- CREATE INDEX `fk_users_groups` ON `users` (`group_id` ASC) ;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`games`
+-- Table `games`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`games` (
+CREATE  TABLE IF NOT EXISTS `games` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `challenger_id` INT NULL DEFAULT 0 ,
   `opponent_id` INT NULL DEFAULT 0 ,
-  `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-  `modified` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `created` DATETIME  DEFAULT NULL, -- CURRENT_TIMESTAMP ,
+  `modified` DATETIME DEFAULT NULL, -- CURRENT_TIMESTAMP ,
   `terminated` TINYINT NULL DEFAULT 0 ,
-  `winner_id` INT NOT NULL DEFAULT 0 ,
+  `winner_id` INT DEFAULT 0 ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_games_users2`
     FOREIGN KEY (`challenger_id` )
-    REFERENCES `mydb`.`users` (`id` )
+    REFERENCES `users` (`id` )
     ON DELETE SET NULL
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_games_users3`
     FOREIGN KEY (`opponent_id` )
-    REFERENCES `mydb`.`users` (`id` )
+    REFERENCES `users` (`id` )
     ON DELETE SET NULL
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_games_users1`
     FOREIGN KEY (`winner_id` )
-    REFERENCES `mydb`.`users` (`id` )
+    REFERENCES `users` (`id` )
     ON DELETE SET NULL
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_games_users2` ON `mydb`.`games` (`challenger_id` ASC) ;
-
-CREATE INDEX `fk_games_users3` ON `mydb`.`games` (`opponent_id` ASC) ;
-
-CREATE INDEX `fk_games_users1` ON `mydb`.`games` (`winner_id` ASC) ;
+-- CREATE INDEX `fk_games_challenger_id` ON `games` (`challenger_id` ASC) ;
+-- CREATE INDEX `fk_games_opponent_id` ON `games` (`opponent_id` ASC) ;
+-- CREATE INDEX `fk_games_winner_id` ON `games` (`winner_id` ASC) ;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`turns`
+-- Table `turns`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`turns` (
+CREATE  TABLE IF NOT EXISTS `turns` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `created` DATETIME DEFAULT NULL, -- CURRENT_TIMESTAMP ,
   `game_id` INT NOT NULL ,
   `creator` INT NULL ,
   `x` TINYINT NOT NULL ,
@@ -90,25 +84,25 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`turns` (
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_turns_games1`
     FOREIGN KEY (`game_id` )
-    REFERENCES `mydb`.`games` (`id` )
+    REFERENCES `games` (`id` )
     ON DELETE RESTRICT
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_turns_users1`
     FOREIGN KEY (`creator` )
-    REFERENCES `mydb`.`users` (`id` )
+    REFERENCES `users` (`id` )
     ON DELETE SET NULL
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_turns_games1` ON `mydb`.`turns` (`game_id` ASC) ;
-
-CREATE INDEX `fk_turns_users1` ON `mydb`.`turns` (`creator` ASC) ;
-
+/*
+CREATE INDEX `fk_turns_game_id` ON `turns` (`game_id` ASC) ;
+CREATE INDEX `fk_turns_creator` ON `turns` (`creator` ASC) ;
+*/
 
 -- -----------------------------------------------------
--- Table `mydb`.`acos`
+-- Table `acos`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`acos` (
+CREATE  TABLE IF NOT EXISTS `acos` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `parent_id` INT(10) NULL DEFAULT NULL ,
   `model` VARCHAR(255) NULL DEFAULT '' ,
@@ -120,9 +114,9 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`acos` (
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`aros`
+-- Table `aros`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`aros` (
+CREATE  TABLE IF NOT EXISTS `aros` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `parent_id` INT(10) NULL DEFAULT NULL ,
   `model` VARCHAR(255) NULL DEFAULT '' ,
@@ -130,20 +124,23 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`aros` (
   `lft` INT(10) NULL DEFAULT NULL ,
   `rght` INT(10) NULL DEFAULT NULL ,
   `foreign_key` INT(10) UNSIGNED NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) ,
+  PRIMARY KEY (`id`)
+/*  ,
   CONSTRAINT `fk_aros_groups1`
     FOREIGN KEY (`foreign_key` )
-    REFERENCES `mydb`.`groups` (`id` )
+    REFERENCES `groups` (`id` )
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION
+	*/
+	);
 
-CREATE INDEX `fk_aros_groups1` ON `mydb`.`aros` (`foreign_key` ASC) ;
+-- CREATE INDEX `fk_aros_groups1` ON `aros` (`foreign_key` ASC) ;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`aros_acos`
+-- Table `aros_acos`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`aros_acos` (
+CREATE  TABLE IF NOT EXISTS `aros_acos` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `aro_id` INT(10) UNSIGNED NOT NULL ,
   `aco_id` INT(10) UNSIGNED NOT NULL ,
@@ -154,24 +151,23 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`aros_acos` (
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_aros_acos_acos1`
     FOREIGN KEY (`aro_id` )
-    REFERENCES `mydb`.`acos` (`id` )
+    REFERENCES `acos` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_aros_acos_aros1`
     FOREIGN KEY (`aco_id` )
-    REFERENCES `mydb`.`aros` (`id` )
+    REFERENCES `aros` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
-
-CREATE INDEX `fk_aros_acos_acos1` ON `mydb`.`aros_acos` (`aro_id` ASC) ;
-
-CREATE INDEX `fk_aros_acos_aros1` ON `mydb`.`aros_acos` (`aco_id` ASC) ;
-
+/*
+CREATE INDEX `fk_aros_acos_aro_id` ON `aros_acos` (`aro_id` ASC) ;
+CREATE INDEX `fk_aros_acos_aco_id` ON `aros_acos` (`aco_id` ASC) ;
+*/
 
 -- -----------------------------------------------------
--- Table `mydb`.`sessions`
+-- Table `sessions`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`sessions` (
+CREATE  TABLE IF NOT EXISTS `sessions` (
   `id` VARCHAR(255) NOT NULL DEFAULT '' ,
   `data` TEXT NULL DEFAULT NULL ,
   `expires` INT(11) NULL DEFAULT NULL ,
@@ -179,33 +175,29 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`sessions` (
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`waitingForGames`
+-- Table `waitingForGames`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`waitingForGames` (
+CREATE  TABLE IF NOT EXISTS `waitingForGames` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `user_id` INT NOT NULL ,
-  `created` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ,
-  `modified` DATETIME NULL ,
+  `created` DATETIME  DEFAULT NULL, -- CURRENT_TIMESTAMP ,
+  `modified` DATETIME  ,
   `session_id` VARCHAR(255) NULL ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_waitingForGames_users1`
     FOREIGN KEY (`user_id` )
-    REFERENCES `mydb`.`users` (`id` )
+    REFERENCES `users` (`id` )
     ON DELETE CASCADE
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_waitingForGames_cake_sessions1`
     FOREIGN KEY (`session_id` )
-    REFERENCES `mydb`.`sessions` (`id` )
+    REFERENCES `sessions` (`id` )
     ON DELETE NO ACTION
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = Memory;
 
-CREATE INDEX `fk_waitingForGames_users1` ON `mydb`.`waitingForGames` (`user_id` ASC) ;
+-- CREATE INDEX `fk_waitingForGames_user_id` ON `waitingForGames` (`user_id` ASC) ;
 
-CREATE INDEX `fk_waitingForGames_cake_sessions1` ON `mydb`.`waitingForGames` (`session_id` ASC) ;
+-- CREATE INDEX `fk_waitingForGames_cake_session_id` ON `waitingForGames` (`session_id` ASC) ;
 
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ backup_letsplay5.sql
@@ -0,0 +1,218 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Erstellungszeit: 05. Jul 2012 um 10:23
+-- Server Version: 5.5.16
+-- PHP-Version: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+01:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `letsplay5`
+--
+
+DROP DATABASE IF EXISTS `letsplay5`;
+CREATE DATABASE IF NOT EXISTS `letsplay5` DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+USE letsplay5;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `acos`
+--
+
+CREATE TABLE IF NOT EXISTS `acos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) DEFAULT NULL,
+  `model` varchar(255) COLLATE utf8_bin DEFAULT '',
+  `foreign_key` int(10) unsigned DEFAULT NULL,
+  `alias` varchar(255) COLLATE utf8_bin DEFAULT '',
+  `lft` int(10) DEFAULT NULL,
+  `rght` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `aros`
+--
+
+CREATE TABLE IF NOT EXISTS `aros` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) DEFAULT NULL,
+  `model` varchar(255) COLLATE utf8_bin DEFAULT '',
+  `alias` varchar(255) COLLATE utf8_bin DEFAULT '',
+  `lft` int(10) DEFAULT NULL,
+  `rght` int(10) DEFAULT NULL,
+  `foreign_key` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `aros_acos`
+--
+
+CREATE TABLE IF NOT EXISTS `aros_acos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `aro_id` int(10) unsigned NOT NULL,
+  `aco_id` int(10) unsigned NOT NULL,
+  `_create` char(2) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `_read` char(2) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `_update` char(2) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `_delete` char(2) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `fk_aros_acos_acos1` (`aro_id`),
+  KEY `fk_aros_acos_aros1` (`aco_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `games`
+--
+
+CREATE TABLE IF NOT EXISTS `games` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `challenger_id` int(11) DEFAULT '0',
+  `opponent_id` int(11) DEFAULT '0',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `terminated` tinyint(4) DEFAULT '0',
+  `winner_id` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `fk_games_users2` (`challenger_id`),
+  KEY `fk_games_users3` (`opponent_id`),
+  KEY `fk_games_users1` (`winner_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_bin NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `data` text COLLATE utf8_bin,
+  `expires` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `turns`
+--
+
+CREATE TABLE IF NOT EXISTS `turns` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime DEFAULT NULL,
+  `game_id` int(11) NOT NULL,
+  `creator` int(11) DEFAULT NULL,
+  `x` tinyint(4) NOT NULL,
+  `y` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_turns_games1` (`game_id`),
+  KEY `fk_turns_users1` (`creator`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) COLLATE utf8_bin NOT NULL DEFAULT 'Gast',
+  `password` varchar(32) COLLATE utf8_bin NOT NULL,
+  `email` varchar(50) COLLATE utf8_bin NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `score` int(11) DEFAULT '0',
+  `isMale` tinyint(4) DEFAULT '1',
+  `group_id` int(11) NOT NULL,
+  `storePassword` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_users_groups` (`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `waitingforgames`
+--
+
+CREATE TABLE IF NOT EXISTS `waitingforgames` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `session_id` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_waitingForGames_users1` (`user_id`),
+  KEY `fk_waitingForGames_cake_sessions1` (`session_id`)
+) ENGINE=MEMORY DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `aros_acos`
+--
+ALTER TABLE `aros_acos`
+  ADD CONSTRAINT `fk_aros_acos_acos1` FOREIGN KEY (`aro_id`) REFERENCES `acos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_aros_acos_aros1` FOREIGN KEY (`aco_id`) REFERENCES `aros` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints der Tabelle `games`
+--
+ALTER TABLE `games`
+  ADD CONSTRAINT `fk_games_users2` FOREIGN KEY (`challenger_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_games_users3` FOREIGN KEY (`opponent_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_games_users1` FOREIGN KEY (`winner_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION;
+
+--
+-- Constraints der Tabelle `turns`
+--
+ALTER TABLE `turns`
+  ADD CONSTRAINT `fk_turns_games1` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_turns_users1` FOREIGN KEY (`creator`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION;
+
+--
+-- Constraints der Tabelle `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_users_groups` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
