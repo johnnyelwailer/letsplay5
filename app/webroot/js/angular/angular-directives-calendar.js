426ee1f7568d389d193966bdb507393f04bb54bb
pj <pj@nexplore.ch>
2012-07-07 20:49:46 +0200

    calendar niiice

--- app/webroot/js/angular/angular-directives-calendar.js
+++ app/webroot/js/angular/angular-directives-calendar.js
@@ -1,110 +1,122 @@
-App.directive('calendar', function() {
-   return {
-     restrict: 'EA',
-     replace: false,
-     scope: {selecteddate: '='},
-     compile: function($place, att) {
-         console.log(arguments);
+App.directive('calendar', function ($compile) {
+    return {
+        restrict: 'EA',
+        replace: false,
+        scope: { selecteddate: '=' },
+        compile: function ($place, att) {
+            var template =
+                '<div class="calendar">' +
+                    '<div class="nav">' +
+                        '<div class="current button">{{currentMonth.date | date:"MMMM"}}</div>' +
+                        '<div class="prev button" ng-click="prevMonth()">prev</div>' +
+                        '<div class="next button" ng-click="nextMonth()">next</div>' +
+                    '</div>' +
+                    '<div class="head">' +
+                        '<div>Mo</div>' +
+                        '<div>Di</div>' +
+                        '<div>Mi</div>' +
+                        '<div>Do</div>' +
+                        '<div>Fr</div>' +
+                        '<div>Sa</div>' +
+                        '<div>So</div>' +
+                    '</div>' +
+                    '<div class="body">' +
+                        '<div class="months">' +
+                            '<div class="days">' +
+                                '<div class="day" ng-repeat="day in currentMonth.days"' +
+                                '                 aria-selected="{{day.getDayDate().valueOf() == selecteddate.getDayDate().valueOf()}}"' +
+                                '                 ng-click="select(day)">' +
+                                   '{{day.getDate()}}' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>';
 
-         var template =
-             '<div class="calendar">' +
-                 '<div class="nav">' +
-                     '<div class="current button">{{currentMonth.date | date:"MMMM"}}</div>' +
-                     '<div class="prev button" ng-click="prevMonth()">prev</div>' +
-                     '<div class="next button" ng-click="nextMonth()">next</div>' +
-                 '</div>' +
-                 '<div class="head">' +
-                     '<div>Mo</div>' +
-                     '<div>Di</div>' +
-                     '<div>Mi</div>' +
-                     '<div>Do</div>' +
-                     '<div>Fr</div>' +
-                     '<div>Sa</div>' +
-                     '<div>So</div>' +
-                 '</div>' +
-                 '<div class="body">' +
-                     '<div class="months">' +
-                         '<div class="days">' +
-                             '<div class="day" ng-repeat="day in currentMonth.days"' +
-                             '                 aria-selected="{{day.getDayDate().valueOf() == selecteddate.getDayDate().valueOf()}}"' +
-                             '                 ng-click="select(day)">' +
-                                '{{day.getDate()}}' +
-                             '</div>' +
-                         '</div>' +
-                     '</div>' +
-                 '</div>' +
-             '</div>';
+            var link = function (scope, $element) {
+                scope.selecteddate = scope.selecteddate || new Date;
 
-         return function(scope, $element) {
-             //please link to scope
-             if ($element.get(0).nodeName == 'INPUT') {
-                $('<popout position="{left: 0; top: 0}">'+template+'</popout>').insertAfter($element);
-             }
-             else {
-                $(template).appendTo($element);
-             }
-         };
+                scope.currentMonth = {
+                    date: new Date,
+                    days: []
+                };
 
-     },
-     link: function(scope, $element) {
-         scope.selecteddate = scope.selecteddate || new Date;
+                scope.nextMonth = function () {
+                    scope.selecteddate = scope.selecteddate.addMonths(1);
+                };
 
-         scope.currentMonth = {
-             date: new Date,
-             days: []
-         };
+                scope.prevMonth = function () {
+                    scope.selecteddate = scope.selecteddate.addMonths(-1);
+                };
 
-         scope.nextMonth = function() {
-             scope.selecteddate = scope.selecteddate.addMonths(1);
-         };
+                scope.select = function (date) {
+                    scope.selecteddate = date;
+                };
 
-         scope.prevMonth = function() {
-             scope.selecteddate = scope.selecteddate.addMonths(-1);
-         };
+                scope.$watch('selecteddate', function (date, oldDate) {
+                    if (date.getMonthDate().valueOf() != oldDate.getMonthDate().valueOf()) {
+                        generateMonth();
+                    }
+                });
 
-         scope.select = function(date) {
-             scope.selecteddate = date;
-         };
+                var loadDays = function (date) {
+                    var lastday = date.lastDayOfMonth().lastDayOfWeek();
+                    var firstday = date.firstDayOfMonth().firstDayOfWeek();
+                    return Array.generate(function (i, prev) {
+                        prev = prev !== null ? prev.addDays(1) : firstday;
+                        var d = prev;
 
-         scope.$watch('selecteddate', function(date, oldDate){
-             if (date.getMonthDate().valueOf() != oldDate.getMonthDate().valueOf()) {
-                 generateMonth();
-             }
-         });
+                        scope.currentMonth.days[i] = d;
 
-         var loadDays = function(date) {
-             var lastday = date.lastDayOfMonth().lastDayOfWeek();
-             var firstday = date.firstDayOfMonth().firstDayOfWeek();
-             return Array.generate(function(i, prev) {
-                 prev = prev !== null ? prev.addDays(1) : firstday;
-                 var d = prev;
+                        return (d - lastday > 0) ? false : d;
+                    });
+                };
 
-                 scope.currentMonth.days[i] = d;
+                var generateMonth = function () {
+                    scope.currentMonth.date = new Date(scope.selecteddate.getFullYear(), scope.selecteddate.getMonth() + 1, 0);
+                    loadDays(scope.currentMonth.date);
+                }
 
-                 return (d - lastday > 0) ? false : d;
-             });
-         };
+                generateMonth();
+            };
 
-         var generateMonth = function() {
-             scope.currentMonth.date =  new Date(scope.selecteddate.getFullYear(), scope.selecteddate.getMonth()+1, 0);
-             loadDays(scope.currentMonth.date);
-         }
+            return function (scope, $element) {
+                if ($element.get(0).nodeName == 'INPUT') {
+                    scope.element = $element;
+                    $compile('<popout element="element">' + template + '</popout>')(scope, function (clone) {
+                        link(scope, clone);
 
-         generateMonth();
-     }
+                        scope.$watch('selecteddate', function (val) {
+                            $element.val(val);
+                        });
 
-   };
+                        $element.change(function (val) {
+                            var date = new Date($element.val())
+                            scope.$apply(function () {
+                                scope.selecteddate = date;
+                            });
+                        });
+
+                        clone.insertAfter($element);
+                    });
+                }
+                else {
+                    $compile(template)(scope, function (clone) {
+                        link(scope, clone);
+                        clone.appendTo($element);
+                    });
+                }
+            };
+        }
+    };
 })
-    .directive('popout', function() {
-        return {
-          restrict: 'E',
-          transclude: true,
-          scope: {position: '='},
-          link: function(scope, $element) {
-          },
-          template:
-              '<div class="popout" ng-style="left: position.left, top: position.top">' +
-               '</div>'
-
-        };
-    });
+.directive('popout', function () {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: { element: '=' },
+        template:
+            '<div class="popout" ng-style="{left: 0, top: 0}" ng-transclude>' +
+            '</div>'
+    };
+});
