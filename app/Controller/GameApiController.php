04253460e47223cd54b2a19e513260f809ced5cc
phil jay <pj@nexplore.ch>
2012-07-11 11:04:53 +0200

    fetter sh it game rules server side win checking

--- app/Controller/GameApiController.php
+++ app/Controller/GameApiController.php
@@ -111,18 +111,23 @@ class GameApiController extends AppController {
 
     public function turns($id, $since = null) {
         $this->loadModel('Turn');
-        
+        $this->loadModel('Game');
+
         $turns = $this->Turn->findAllByGameId($id);
+
+        $game = $this->Game->findById($id);
 
         $this->set(array(
             'turns' => $turns,
-            '_serialize' => array('turns')
+            'winner' => $game['Game']['winner_id'],
+            '_serialize' => array('turns', 'winner')
         ));
     }
 
     public function place($id, $x, $y) {
         $this->loadModel('Turn');
         $this->loadModel('User');
+        $this->loadModel('Game');
 
         $existing = $this->Turn->find('first', array('conditions' =>
             array('game_id' => $id, 'x' => $x, 'y' => $y)));
@@ -133,9 +138,22 @@ class GameApiController extends AppController {
 
         $user = $this->currentUser();
         $turn = $this->Turn->save(array('game_id' => $id, 'x' => $x, 'y' => $y, 'creator' => $user['id']));
+
+        $game = new GameMaths($this->Turn, $id);
+
+        $rows = $game->findAdjacentRows($turn);
+        $won = count($rows) > 0;
+
+        if($won) {
+            $this->Game->id = $id;
+            $this->Game->save(array('terminated' => true, 'winner_id' => $user['id']));
+        }
+
         $this->set(array(
             'turn' => $turn,
-            '_serialize' => array('turn')
+            'won' => $won,
+            'rows' => $rows,
+            '_serialize' => array('turn', 'won', 'rows')
         ));
     }
 
@@ -216,4 +234,69 @@ class GameApiController extends AppController {
 	}
 
 
+}
+
+class GameMaths {
+    public $turns;
+
+    public function __construct($turns, $game){
+        $this->turns = $turns;
+        $this->game = $game;
+    }
+
+    public function getTurnAt($pos) {
+        return $this->turns->find('first', array(
+            'conditions' => array(
+                'x' => $pos['x'],
+                'y' => $pos['y'],
+                'game_id' => $this->game
+            )
+        ));
+    }
+
+    public function isOccupied($pos, $by) {
+        $turn = $this->getTurnAt($pos);
+
+        if ($turn == false)
+            return false;
+
+        return $turn['Turn']['creator'] == $by;
+    }
+
+    public function getTurnsInLine($turn, $vector) {
+        $result = array($turn);
+        $creator = $turn['Turn']['creator'];
+
+        $current = $this->getTurnAt(array('x' => $turn['Turn']['x'] + $vector['x'],'y' => $turn['Turn']['y'] + $vector['y']));
+        while ($this->isOccupied($current['Turn'], $creator)) {
+            array_push($result, $current);
+            $current = $this->getTurnAt(array('x' => $current['Turn']['x'] + $vector['x'],'y' => $current['Turn']['y'] + $vector['y']));
+        }
+
+        $current = $this->getTurnAt(array('x' => $turn['Turn']['x'] - $vector['x'],'y' => $turn['Turn']['y'] - $vector['y']));
+        while ($this->isOccupied($current['Turn'], $creator)) {
+            array_push($result, $current);
+            $current = $this->getTurnAt(array('x' => $current['Turn']['x'] - $vector['x'],'y' => $current['Turn']['y'] - $vector['y']));
+        }
+
+        return $result;
+    }
+
+    public function findAdjacentRows($turn) {
+        $result = array();
+        $allRows = array(
+            $this->getTurnsInLine($turn, array('x'=> 1, 'y' => 0)),
+            $this->getTurnsInLine($turn, array('x'=> 1, 'y' => 1)),
+            $this->getTurnsInLine($turn, array('x'=> 0, 'y' => 1)),
+            $this->getTurnsInLine($turn, array('x'=> -1, 'y' => 1))
+        );
+
+        foreach($allRows as $row) {
+            if (count($row) >= 5) {
+                array_push($result, $row);
+            }
+        }
+
+        return $result;
+    }
 }
--- app/View/Games/play.ctp
+++ app/View/Games/play.ctp
@@ -9,17 +9,27 @@ echo $this->Html->css('game');
 
 ?>
 <div ng-controller="GameViewModel" class="play-grid">
-    <div class="message transitioned" ng-class="{collapsedY: !waitingForOpponent}">
-        <h1>Waiting for opponent</h1>
+    <div ng-show="!isCompleted()" >
+        <div class="message transitioned" ng-class="{collapsedY: !waitingForOpponent}">
+            <h1>Waiting for opponent</h1>
+        </div>
+        <div class="message transitioned" ng-class="{collapsedY: isMyTurn}">
+            <h1>It's the opponents turn...</h1>
+        </div>
     </div>
-    <div class="message transitioned" ng-class="{collapsedY: isMyTurn}">
-        <h1>It's the opponents turn...</h1>
+    <div ng-show="isCompleted()" >
+        <div class="message transitioned collapsedY" ng-class="{collapsedY: !hasWon()}">
+            <h1>You won!</h1>
+        </div>
+        <div class="message transitioned" ng-class="{collapsedY: hasWon()}">
+            <h1>You lost!</h1>
+        </div>
     </div>
+
     <div class="grid transitioned" ng-class="{collapsedY: waitingForOpponent, 'my-turn': isMyTurn}">
         <div ng-repeat="turn in grid" class="grid-cell transitioned"
              ng-click="place($index)">
-            <div class="turn transitioned" ng-class="{marked: isMarked(turn), 'by-me': turn.isMine, 'belongs-to-line': turn.completedLines.length > 0}">
-                {{turn.creator || '('+$index+')'}}
+            <div class="turn transitioned " ng-class="{marked: isMarked(turn), 'by-me': turn.isMine, 'belongs-to-line': turn.completedLines.length > 0}">
             </div>
 
         </div>
